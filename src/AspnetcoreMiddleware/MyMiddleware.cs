931ef60531f7540195398dca920769caa6eccdb6
tonykung <tonykung06@gmail.com>
2017-08-05 13:16:16 +0800

    Middleware class

---
+++ src/AspnetcoreMiddleware/MyMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+namespace AspnetcoreMiddleware
+{
+    public class MyMiddleware
+    {
+        private RequestDelegate _next;
+        private ILoggerFactory _loggerFactory;
+        private IOptions<MyMiddlewareOptions> _options;
+
+        public MyMiddleware(
+            RequestDelegate next,
+            ILoggerFactory loggerFactory,
+            IOptions<MyMiddlewareOptions> options)
+        {
+            _options = options;
+            _loggerFactory = loggerFactory;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _loggerFactory.AddConsole();
+            var logger = _loggerFactory.CreateLogger("MyMiddleware");
+            logger.LogInformation("testing", _options.Value.OptionOne);
+
+            await context.Response.WriteAsync($"hello from MyMiddleware, {_options.Value.OptionOne}, ");
+            await _next.Invoke(context);
+        }
+    }
+
+    public class MyMiddlewareOptions
+    {
+        public string OptionOne { get; set; }
+    }
+
+    public static class MyMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMyMiddleware(this IApplicationBuilder appBuilder)
+        {
+            return appBuilder.UseMiddleware<MyMiddleware>();
+        }
+    }
+}
--- src/AspnetcoreMiddleware/Startup.cs
+++ src/AspnetcoreMiddleware/Startup.cs
@@ -7,15 +7,29 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace AspnetcoreMiddleware
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(env.ContentRootPath)
+                            .AddJsonFile("appsettings.json", false, true);
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions();
+            var myMiddlewareOptions = Configuration.GetSection("MyMiddlewareSection");
+            services.Configure<MyMiddlewareOptions>(option => option.OptionOne = myMiddlewareOptions["OptionOne"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +69,8 @@ namespace AspnetcoreMiddleware
                     context.Response.WriteAsync("hey from mymapwhen, ");
                 });
             });
+
+            app.UseMyMiddleware();
 
             // app.Run is pipeline terminator
             // this will never be executed when it branches out into /mymapbranch
